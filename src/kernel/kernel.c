896eb9dcc5d736072aa46e107680c1f719e0396c
rcastill <rodolfocastillomateluna@gmail.com>
2015-06-15 17:19:00 -0300

    Convention changes.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -15,7 +15,8 @@
 #endif
  
 /* Hardware text mode color constants. */
-enum vga_color {
+enum vga_color
+{
 	COLOR_BLACK = 0,
 	COLOR_BLUE = 1,
 	COLOR_GREEN = 2,
@@ -34,19 +35,22 @@ enum vga_color {
 	COLOR_WHITE = 15,
 };
  
-uint8_t make_color(enum vga_color fg, enum vga_color bg) {
+uint8_t make_color(enum vga_color fg, enum vga_color bg)
+{
 	return fg | bg << 4;
 }
  
-uint16_t make_vgaentry(char c, uint8_t color) {
+uint16_t make_vgaentry(char c, uint8_t color)
+{
 	uint16_t c16 = c;
 	uint16_t color16 = color;
 	return c16 | color16 << 8;
 }
  
-size_t strlen(const char* str) {
+size_t strlen(const char* str)
+{
 	size_t ret = 0;
-	while ( str[ret] != 0 )
+	while (str[ret] != 0)
 		ret++;
 	return ret;
 }
@@ -59,11 +63,13 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
  
-void terminal_initialize() {
+void terminal_initialize()
+{
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
+	
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -72,35 +78,40 @@ void terminal_initialize() {
 	}
 }
  
-void terminal_setcolor(uint8_t color) {
+void terminal_setcolor(uint8_t color)
+{
 	terminal_color = color;
 }
  
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
+{
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = make_vgaentry(c, color);
 }
  
 void terminal_putchar(char c) {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
+		
 		if (++terminal_row == VGA_HEIGHT) {
 			terminal_row = 0;
 		}
 	}
 }
  
-void terminal_writestring(const char* data) {
+void terminal_writestring(const char* data)
+{
 	size_t datalen = strlen(data);
 	for (size_t i = 0; i < datalen; i++)
 		terminal_putchar(data[i]);
 }
  
 #if defined(__cplusplus)
-extern "C" /* Use C linkage for kernel_main. */
+extern "C" /* Use C linkage for kmain. */
 #endif
-void kernel_main() {
+void kmain() {
 	/* Initialize terminal interface */
 	terminal_initialize();
  
