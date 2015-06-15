7b6350538f11e9d2881d2110f1c2bed5a41c49c7
rcastill <rodolfocastillomateluna@gmail.com>
2015-06-15 17:20:45 -0300

    Fix da kernel.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -62,23 +62,14 @@ size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
-<<<<<<< HEAD
  
 void terminal_initialize()
 {
-=======
-
-void terminal_initialize() {
->>>>>>> 44972b4975fbb845212e02b4365454873ce538e7
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
-<<<<<<< HEAD
 	
-=======
-    
->>>>>>> 44972b4975fbb845212e02b4365454873ce538e7
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -98,20 +89,12 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	terminal_buffer[index] = make_vgaentry(c, color);
 }
  
-void terminal_putchar(char c)
-{
+void terminal_putchar(char c) {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-<<<<<<< HEAD
 	
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		
-=======
-    
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-        
->>>>>>> 44972b4975fbb845212e02b4365454873ce538e7
 		if (++terminal_row == VGA_HEIGHT) {
 			terminal_row = 0;
 		}
@@ -121,10 +104,8 @@ void terminal_putchar(char c)
 void terminal_writestring(const char* data)
 {
 	size_t datalen = strlen(data);
-    
-    for (size_t i = 0; i < datalen; i++) {
-        terminal_putchar(data[i]);
-    }
+	for (size_t i = 0; i < datalen; i++)
+		terminal_putchar(data[i]);
 }
  
 #if defined(__cplusplus)
@@ -140,26 +121,3 @@ void kmain() {
          */
 	terminal_writestring("Hello, kernel World!\n");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
