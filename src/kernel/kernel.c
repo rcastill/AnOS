7bfb611f7fb3e6176c5991348d85c864ffcc1e61
Marcelo Jara Almeyda <marjara35@gmail.com>
2015-06-15 17:31:13 -0300

    Add newline support.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -34,19 +34,19 @@ enum vga_color
 	COLOR_LIGHT_BROWN = 14,
 	COLOR_WHITE = 15,
 };
- 
+
 uint8_t make_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
- 
+
 uint16_t make_vgaentry(char c, uint8_t color)
 {
 	uint16_t c16 = c;
 	uint16_t color16 = color;
 	return c16 | color16 << 8;
 }
- 
+
 size_t strlen(const char* str)
 {
 	size_t ret = 0;
@@ -54,10 +54,10 @@ size_t strlen(const char* str)
 		ret++;
 	return ret;
 }
- 
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
- 
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
@@ -69,7 +69,7 @@ void terminal_initialize()
 	terminal_column = 0;
 	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
-	
+
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -77,25 +77,30 @@ void terminal_initialize()
 		}
 	}
 }
- 
+
 void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
+
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = make_vgaentry(c, color);
 }
- 
+
 void terminal_putchar(char c)
 {
+    if (c == '\n') {
+        terminal_row++;
+        return;
+    }
+
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	
+
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
-		
+
 		if (++terminal_row == VGA_HEIGHT) {
 			terminal_row = 0;
 		}
@@ -109,7 +114,7 @@ void terminal_writestring(const char* data)
 	for (size_t i = 0; i < datalen; i++)
 		terminal_putchar(data[i]);
 }
- 
+
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kmain. */
 #endif
